c621e5bdef67f13e60f83b6dc141f615deb47603
Collect999 <dregz299@gmail.com>
2023-10-21 14:52:18 +0100

    adding debug mode

--- SoundSwitch.py
+++ SoundSwitch.py
@@ -12,14 +12,24 @@ from scipy.signal import butter, filtfilt
 import time
 import pyautogui
 
+# Command-line argument parsing
+parser = argparse.ArgumentParser(description='Sound Switch')
+parser.add_argument('--debug', action='store_true', help='Enable debug mode')
+args = parser.parse_args()
+
+# Enable logging if debug flag is set
+if args.debug:
+	logging.basicConfig(filename='debug.log', level=logging.DEBUG)
+	print("Debug mode enabled.")
+
 ahh_templates = []
 
 # Determine application and config paths
 if getattr(sys, 'frozen', False):
-    home_directory = os.path.expanduser("~")
-    application_path = os.path.join(home_directory, 'AppData', 'Roaming', 'SoundSwitch')
+	home_directory = os.path.expanduser("~")
+	application_path = os.path.join(home_directory, 'AppData', 'Roaming', 'SoundSwitch')
 elif __file__:
-    application_path = os.path.dirname(__file__)
+	application_path = os.path.dirname(__file__)
 config_path = os.path.join(application_path, 'config.ini')
 sample_folder = os.path.join(application_path, 'sound-samples')
 
@@ -32,119 +42,139 @@ key_to_press = config['DEFAULT']['KeyToPress']
 audioinput = int(config['DEFAULT']['AudioInput'])
 
 def load_samples():
-    global ahh_templates  # Declare ahh_templates as global
-    sample_files = [f for f in os.listdir(sample_folder) if f.endswith('.wav')]
-    for f in sample_files:
-        audio, sr = librosa.load(os.path.join(sample_folder, f), sr=44100)
-        ahh_templates.append(audio)
-    return sr  # return sample rate
+	global ahh_templates  # Declare ahh_templates as global
+	sample_files = [f for f in os.listdir(sample_folder) if f.endswith('.wav')]
+	for f in sample_files:
+		audio, sr = librosa.load(os.path.join(sample_folder, f), sr=44100)
+		ahh_templates.append(audio)
+	return sr  # return sample rate
 
 # Helper function to find the audio devices. 
 def findAudioDevices():
-    p = pyaudio.PyAudio()
-    info = p.get_host_api_info_by_index(0)
-    numdevices = info.get('deviceCount')
-    device_info = ""
-    for i in range(0, numdevices):
-        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-            device_info += f"Input Device ID {i} - {p.get_device_info_by_host_api_device_index(0, i).get('name')}\n"
-    sg.popup('Audio Devices', device_info)
+	p = pyaudio.PyAudio()
+	info = p.get_host_api_info_by_index(0)
+	numdevices = info.get('deviceCount')
+	device_info = ""
+	for i in range(0, numdevices):
+		if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+			device_info += f"Input Device ID {i} - {p.get_device_info_by_host_api_device_index(0, i).get('name')}\n"
+	sg.popup('Audio Devices', device_info)
 
 # Function to perform cross-correlation
 def detect_ahh(audio_signal, templates, threshold):
-    max_correlations = []
-    for template in templates:
-        c = np.correlate(audio_signal, template, mode='valid')
-        max_correlations.append(np.max(c))
-    max_correlation = np.max(max_correlations)
-    return max_correlation > threshold
+	max_correlations = []
+	for template in templates:
+		c = np.correlate(audio_signal, template, mode='valid')
+		max_correlations.append(np.max(c))
+	max_correlation = np.max(max_correlations)
+	return max_correlation > threshold
 
 # The function running the detection loop
 def detection_loop(windo, sr):
-    global stop_thread
-    global ahh_templates
-    stop_thread = False  # Reset when function starts
-    p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paFloat32,
-                    channels=1,
-                    rate=sr,
-                    input=True,
-                    input_device_index=audioinput,
-                    frames_per_buffer=2048)
-    last_triggered_time = 0  # Initialize to zero for first loop
-    cooldown_time = 2.0  # Cooldown time in seconds
+	global stop_thread
+	global ahh_templates
+	stop_thread = False	 # Reset when function starts
+	p = pyaudio.PyAudio()
+	
+	if args.debug:
+		logging.debug(f"Initializing stream with input_device_index: {audioinput}, rate: {sr}")
 
-    while True:
-        if stop_thread:
-            stream.stop_stream()  # Stop the audio stream
-            stream.close()  # Close the audio stream
-            p.terminate()  # Terminate the PyAudio object
-            break  # Break out of the loop
-        block = stream.read(2048, exception_on_overflow=False)
-        audio_signal = np.frombuffer(block, dtype=np.float32)
-        current_time = time.time()
-        max_correlation_value = detect_ahh(audio_signal, ahh_templates, correlation_threshold)
-        if debug:
-            print(f"Max Correlation Value: {max_correlation_value}, Timestamp: {current_time}")
-        if max_correlation_value > correlation_threshold:
-            if current_time - last_triggered_time > cooldown_time:
-                if debug:
-                    print("Ahh sound detected!")
-                pyautogui.press(key_to_press)
-                windo.change_icon(r'IconOn.png')  # Set this variable to your inverted icon
-                # After a short delay, revert back to the original icon
-                time.sleep(0.5)
-                windo.change_icon(r'Icon.png')                
-                last_triggered_time = current_time
-                # Here you can send a message to your GUI or trigger other actions
+	
+	stream = p.open(format=pyaudio.paFloat32,
+					channels=1,
+					rate=sr,
+					input=True,
+					input_device_index=audioinput,
+					frames_per_buffer=2048)
+	last_triggered_time = 0	 # Initialize to zero for first loop
+	cooldown_time = 2.0	 # Cooldown time in seconds
+	
+	if args.debug:
+		logging.debug("Stream initialized.")
+	
+
+	while True:
+		if stop_thread:
+			if args.debug:
+				logging.debug("About to terminate stream.")
+			stream.stop_stream()  # Stop the audio stream
+			stream.close()	# Close the audio stream
+			p.terminate()  # Terminate the PyAudio object
+			break  # Break out of the loop
+		try:
+			block = stream.read(2048, exception_on_overflow=False)
+		except Exception as e:
+			if args.debug:
+				logging.debug(f"Exception caught while reading stream: {e}")	
+		audio_signal = np.frombuffer(block, dtype=np.float32)
+		current_time = time.time()
+		max_correlation_value = detect_ahh(audio_signal, ahh_templates, correlation_threshold)
+		if debug:
+			print(f"Max Correlation Value: {max_correlation_value}, Timestamp: {current_time}")
+		if max_correlation_value > correlation_threshold:
+			if current_time - last_triggered_time > cooldown_time:
+				if debug:
+					print("Ahh sound detected!")
+				pyautogui.press(key_to_press)
+				windo.change_icon(r'IconOn.png')  # Set this variable to your inverted icon
+				# After a short delay, revert back to the original icon
+				time.sleep(0.5)
+				windo.change_icon(r'Icon.png')				  
+				last_triggered_time = current_time
+				# Here you can send a message to your GUI or trigger other actions
 
 def initialize_program():
-    global stop_thread, t
-    # PySimpleGUI setup
-    menu_def = ['File', ['Show Audio Devices', 'Open Config', 'Open Sound Files', 'Reload Samples','Exit']]
-    tooltip = 'Tooltip'
-    layout = [[sg.T('Empty Window', key='-T-')]]
-    window = sg.Window('Window Title', layout, finalize=True, enable_close_attempted_event=True, alpha_channel=0)
-    window.hide()
+	global stop_thread, t
+	
+	# Debug log example
+	if args.debug:
+		logging.debug("initialize_program() called")
+		
+	# PySimpleGUI setup
+	menu_def = ['File', ['Show Audio Devices', 'Open Config', 'Open Sound Files', 'Reload Samples','Exit']]
+	tooltip = 'Tooltip'
+	layout = [[sg.T('Empty Window', key='-T-')]]
+	window = sg.Window('Window Title', layout, finalize=True, enable_close_attempted_event=True, alpha_channel=0)
+	window.hide()
 
-    
-    # Initialize the tray
-    tray = SystemTray(menu=menu_def, single_click_events=False, tooltip=tooltip, window=window, icon=r'Icon.png')
-    tray.show_message('Sound Switch', 'Sound Switchs Started!')
+	
+	# Initialize the tray
+	tray = SystemTray(menu=menu_def, single_click_events=False, tooltip=tooltip, window=window, icon=r'Icon.png')
+	tray.show_message('Sound Switch', 'Sound Switchs Started!')
 
-    sr = load_samples()  # get the sample rate    
-    # Start detection loop in a separate thread
-    stop_thread = True
-    if 't' in globals():  # Check if the thread exists
-        t.join()  # Wait for the existing thread to finish
-    t = threading.Thread(target=detection_loop, args=(tray, sr))
-    t.start()
-    stop_thread = False
-    
-    # Event loop
-    while True:
-        event, values = window.read()
-        menu_item = values[event]
-        if menu_item == 'Reload Samples':
-            load_samples()  # Reload the audio samples
-            stop_thread = True
-            t.join()  # Wait for the existing thread to finish
-            stop_thread = False
-            t = threading.Thread(target=detection_loop, args=(tray,sr))
-            t.start()  # Start a new detection thread
-        elif menu_item == 'Exit':
-            stop_thread = True  # Signal to stop the detection_loop thread
-            if 't' in globals():  # Check if the thread exists
-                    t.join()  # Wait for the existing thread to finish
-                    break  # Then break out of the event loop
-        elif menu_item == 'Show Audio Devices':
-            findAudioDevices()  # This function should probably be modified to display a popup instead of printing to console
-        elif menu_item == 'Open Config':
-            os.system(f"notepad {config_path}")
-        elif menu_item == 'Open Sound Files':
-            os.startfile(sample_folder)
+	sr = load_samples()	 # get the sample rate	  
+	# Start detection loop in a separate thread
+	stop_thread = True
+	if 't' in globals():  # Check if the thread exists
+		t.join()  # Wait for the existing thread to finish
+	t = threading.Thread(target=detection_loop, args=(tray, sr))
+	t.start()
+	stop_thread = False
+	
+	# Event loop
+	while True:
+		event, values = window.read()
+		menu_item = values[event]
+		if menu_item == 'Reload Samples':
+			load_samples()	# Reload the audio samples
+			stop_thread = True
+			t.join()  # Wait for the existing thread to finish
+			stop_thread = False
+			t = threading.Thread(target=detection_loop, args=(tray,sr))
+			t.start()  # Start a new detection thread
+		elif menu_item == 'Exit':
+			stop_thread = True	# Signal to stop the detection_loop thread
+			if 't' in globals():  # Check if the thread exists
+					t.join()  # Wait for the existing thread to finish
+					break  # Then break out of the event loop
+		elif menu_item == 'Show Audio Devices':
+			findAudioDevices()	# This function should probably be modified to display a popup instead of printing to console
+		elif menu_item == 'Open Config':
+			os.system(f"notepad {config_path}")
+		elif menu_item == 'Open Sound Files':
+			os.startfile(sample_folder)
 
-    tray.close()
-    window.close()
+	tray.close()
+	window.close()
 
 initialize_program()
